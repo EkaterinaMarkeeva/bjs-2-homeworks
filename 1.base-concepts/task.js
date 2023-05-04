7ba89e80512abfd6ac0fd2b39d1b0416363acd05
Ekaterina Markeeva <lesekaterina25@gmail.com>
2023-05-04 14:30:17 +0400

    homework 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,40 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = b ** 2 - 4 * a * c;
+
+	if (d > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a));
+		arr.push((-b - Math.sqrt(d)) / (2 * a));
+	} else if (d === 0) {
+		arr.push(-b / (2 * a));
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	if (typeof percent !== Number) {
+		percent = Number(percent);
+	}
+
+	if (typeof contribution !== Number) {
+		contribution = Number(contribution);
+	}
+
+	if (typeof amount !== Number) {
+		amount = Number(amount);
+	}
+
+	if (typeof countMonths !== Number) {
+		countMonths = Number(countMonths);
+	}
+
+	let principalDebt = amount - contribution;
+	let percentMonth = percent / 100 / 12;
+	let payment = principalDebt * (percentMonth + (percentMonth / (((1 + percentMonth) ** countMonths) - 1)));
+	let sum = payment * countMonths;
+
+	return Number(sum.toFixed(2));
 }
