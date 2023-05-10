a7f4cde67fb09fab048e1fe1228bade23b73208b
Ekaterina Markeeva <lesekaterina25@gmail.com>
2023-05-10 12:46:56 +0400

    homework 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,44 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks !== undefined) {
+        this.marks.push(...marks);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks === undefined || this.marks.length === 0) {
+        return 0;
+    }
+    
+    let averageValue = this.marks.reduce((acc, item, index, marks) => {
+        acc += item;
+        let marksLength = marks.length;
+        
+        if (index === marksLength - 1) {
+            return acc / marksLength;
+        }
+
+        return acc
+    }, 0);
+
+    return averageValue;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks
+    this.excluded = reason;
 }
+
+let student1 = new Student("Василиса", "женский", 19);
+let student2 = new Student("Артём", "мужской", 25);
