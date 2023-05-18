51ab2d47b89f22cc246bad607de753e78a0c8b45
Ekaterina Markeeva <lesekaterina25@gmail.com>
2023-05-18 13:33:42 +0400

    homework 6

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,53 @@
-﻿
+﻿function parseCount(quantity) {
+	let result = Number.parseFloat(quantity);
+	if (isNaN(result)) {
+		const error = new Error("Невалидное значение");
+		throw error;
+	}
+	return result;
+}
+
+function validateCount(quantity) {
+	try {
+		return parseCount(quantity);
+	} catch (error) {
+		return error;
+	}
+}
+
+class Triangle {
+	constructor(a, b, c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+        if (a + b < c || b + c < a || a + c < b) {
+            const error = new Error("Треугольник с такими сторонами не существует");
+            throw error;
+	    }
+	}
+
+	get perimeter() {
+		let perimeter = this.a + this.b + this.c;
+		return perimeter;
+	}
+
+	get area() {
+		let halfMeter = (this.a + this.b + this.c) / 2;
+		let square = Math.sqrt(halfMeter * (halfMeter - this.a) * (halfMeter - this.b) * (halfMeter - this.c));
+		return Number(square.toFixed(3));
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+    function triangleGenerator() {
+      return new Triangle(a, b, c);
+    }
+    return new Triangle(a, b, c);
+	} catch (error) {
+		return {
+			get area() {return "Ошибка! Треугольник не существует"} ,
+            get perimeter() {return "Ошибка! Треугольник не существует"} ,
+		};
+	} 
+}
